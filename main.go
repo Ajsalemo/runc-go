ba5d60758d11ef4f960149fd6439a42e13f243dc
Anthony Salemo <ajssalemo@gmail.com>
2025-08-31 15:29:15 -0400

    updating oci bundle for testing

--- main.go
+++ main.go
@@ -11,6 +11,26 @@ type Conf struct {
 	Enabled *bool
 }
 
+func listContainers(r runc.Runc, ctx context.Context) error {
+	list, err := r.List(ctx)
+	if err != nil {
+		return fmt.Errorf("error listing containers: %w", err)
+	}
+	// Check if there are 0 containers. If so, return earlier
+	if len(list) == 0 {
+		fmt.Println("No containers found")
+		return nil
+	}
+	// Loop through the returned results. Type of list is []runc.Container
+	for _, container := range list {
+		fmt.Println("Container ID:", container.ID)
+		fmt.Println("Container Pid:", container.Pid)
+		fmt.Println("Container Bundle:", container.Bundle)
+		fmt.Println("Container rootfs:", container.Rootfs)
+	}
+	return nil
+}
+
 func main() {
 	// Runc configurations
 	isRootless := false
@@ -20,21 +40,5 @@ func main() {
 		Rootless: &isRootless,
 	}
 	// List all containers actively managed by runc
-	list, err := r.List(ctx)
-	if err != nil {
-		fmt.Println("Error listing containers:", err)
-		return
-	}
-	// Check if there are 0 containers. If so, return earlier
-	if len(list) == 0 {
-		fmt.Println("No containers found")
-		return
-	}
-	// Loop through the returned results. Type of list is []runc.Container
-	for _, container := range list {
-		fmt.Println("Container ID:", container.ID)
-		fmt.Println("Container Pid:", container.Pid)
-		fmt.Println("Container Bundle:", container.Bundle)
-		fmt.Println("Container rootfs:", container.Rootfs)
-	}
+	listContainers(r, ctx)
 }
